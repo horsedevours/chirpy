005680a554dc4970dae19b0654832d2ddfcce972
Jonathan Fuller <fullerjn@gmail.com>
2025-05-22 12:30:33 -0500

    Added things

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"sync/atomic"
+)
+
+func main() {
+	cfg := &apiConfig{}
+	smx := http.NewServeMux()
+	smx.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
+	smx.Handle("/app/assets/logo.png", cfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
+	smx.HandleFunc("/healthz", handlerHealthz)
+	smx.HandleFunc("/metrics", cfg.handlerMetrics)
+	smx.HandleFunc("/reset", cfg.handlerReset)
+
+	srvr := http.Server{
+		Handler: smx,
+		Addr:    ":8080",
+	}
+
+	srvr.ListenAndServe()
+}
+
+func handlerHealthz(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(int32(1))
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %s", strconv.Itoa(int(cfg.fileserverHits.Load())))))
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Swap(int32(0))
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
