f6252e37d44101b462253d541270560a78ae4b87
Jonathan Fuller <fullerjn@gmail.com>
2025-05-23 07:01:14 -0500

    Changes

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
-	"strconv"
+	"strings"
 	"sync/atomic"
 )
 
@@ -11,9 +13,10 @@ func main() {
 	cfg := &apiConfig{}
 	smx := http.NewServeMux()
 	smx.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
-	smx.HandleFunc("/healthz", handlerHealthz)
-	smx.HandleFunc("/metrics", cfg.handlerMetrics)
-	smx.HandleFunc("/reset", cfg.handlerReset)
+	smx.HandleFunc("GET /admin/metrics", cfg.handlerMetrics)
+	smx.HandleFunc("POST /admin/reset", cfg.handlerReset)
+	smx.HandleFunc("GET /api/healthz", handlerHealthz)
+	smx.HandleFunc("POST /api/validate_chirp", handlerValidateChirp)
 
 	srvr := http.Server{
 		Handler: smx,
@@ -24,9 +27,45 @@ func main() {
 }
 
 func handlerHealthz(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Add("Content-Type", "text/html")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
+}
+
+func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
+	chirp := struct {
+		Body string `json:"body"`
+	}{}
+
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&chirp)
+	if err != nil {
+		log.Printf("Error decoding JSON: %v", err)
+		w.WriteHeader(500)
+		w.Write([]byte("{\"error\":\"Something went wrong\"}"))
+		return
+	}
+
+	if len(chirp.Body) > 140 {
+		log.Printf("Chirp is too long: %s", chirp.Body)
+		w.WriteHeader(400)
+		w.Write([]byte("{\"error\":\"Chirp is too long\"}"))
+		return
+	}
+
+	badWords := map[string]struct{}{"kerfuffle": struct{}{}, "sharbert": struct{}{}, "fornax": struct{}{}}
+	chirpWords := strings.Split(chirp.Body, " ")
+
+	for i, word := range chirpWords {
+		if _, ok := badWords[strings.ToLower(word)]; ok {
+			chirpWords[i] = "****"
+		}
+	}
+
+	cleanedWords := strings.Join(chirpWords, " ")
+	w.WriteHeader(200)
+	w.Write([]byte(fmt.Sprintf("{\"cleaned_body\":\"%s\"}", cleanedWords)))
+	return
 }
 
 type apiConfig struct {
@@ -41,9 +80,17 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Add("Content-Type", "text/html")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf("Hits: %s", strconv.Itoa(int(cfg.fileserverHits.Load())))))
+	html := `
+<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>	
+	`
+	w.Write([]byte(fmt.Sprintf(html, int(cfg.fileserverHits.Load()))))
 }
 
 func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
